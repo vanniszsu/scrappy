062487f09ac10d650ccb94429aac4b10321998f0
Vannis Yu <vannisyu@gmail.com>
2017-08-31 15:15:21 +0800

    complete v1.0

--- taobaomm.py
+++ taobaomm.py
@@ -29,13 +29,13 @@ class Oss2bucket:
 			
 
 class Spider:
-	def __init__{self}:
+	def __init__(self):
 		self.siteURL = "https://mm.taobao.com/tstar/search/tstar_model.do"
 		
 	def getPage(self,pageindex):
 		payload = {'_input_charset':'utf-8','currentPage':pageindex}
 		page = requests.post(self.siteURL, params=payload)
-		if page.status_code == 200
+		if page.status_code == 200:
 			return page.text
 		else:
 			sys.exit("Cannot get the web page")
@@ -45,15 +45,27 @@ class Spider:
 		regex = '"cardUrl":"(//.+?)".*?"realName":"(.+?)",.*?"userId":(.*?),"'
 		pattern = re.compile(regex, re.S)
 		items = re.findall(pattern, page)
-		
-	def saveimage(self,image):
+		return items
 	
 	def addimages(self,number,start_page=1):
 		count = 0
 		pageindex = start_page
-		
+		oss = Oss2bucket()
+		bucket = oss.getbucket()
 		while count < number:
 			items = self.getAllimages(pageindex)
 			for item in items:
 				imageURL = "https:" + item[0]
-				
+				filename = item[1] + '-' + item[2] + ".jpg"
+				if not(bucket.object_exists(filename)):
+					img = requests.get(imageURL)
+					if img.status_code == 200:
+						result = bucket.put_object(filename, img)
+						if result.status == 200:
+							count += 1
+						else:
+							print "Cannot save " + filename + " to Aliyun OSS"
+					else:
+						print "Cannot download the image " + filename
+			pageindex += 1
+			
